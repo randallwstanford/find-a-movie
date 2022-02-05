a217b928213c32f073a6eff8bea25a8094358a18
randallwstanford <randallwstanford@gmail.com>
2022-02-05 16:16:52 -0700

    Adds get request to movie database.

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -16,12 +17,39 @@ func getPort() string {
 	return port
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	movieTitle := r.FormValue("movieTitle")
+
+	fmt.Fprintf(w, "Movie Title = %s\n", movieTitle)
+
+	url := "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/" + movieTitle
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("x-rapidapi-host", "imdb-internet-movie-database-unofficial.p.rapidapi.com")
+	req.Header.Add("x-rapidapi-key", "c961afde0bmsh919ccbd34b5ebb3p13f56ajsnc1d975d8de37")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+	fmt.Fprintf(w, string(body))
+
 }
 
 func main() {
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
 
 	port := getPort()
 
