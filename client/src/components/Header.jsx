1941f45e447936eedd58dcb1e0906b96c2afb222
randallwstanford <randallwstanford@gmail.com>
2022-02-08 11:50:31 -0700

    Changes function declaration from arrow to standard.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const Header = () => {
+function Header() {
   return (
-    <div className="header">
+    <h1 className="header">
       Find a movie or TV show
-    </div>
-  )
+    </h1>
+  );
 }
 
-export default Header
+export default Header;
